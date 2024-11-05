5c2472aa6e96138f744fc0720dc4373434f922fc
Lia Nirta <lianirta@MacBook-Pro.local>
2024-11-04 21:12:58 -0500

    finished refresher and conditional js labs

---
+++ Refresher_Lab/script.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+const firstName = "lia"
+const lastName = "Nirta"
+const fullName = firstName + " " + lastName
+console.log(fullName)
+
+const population = 633218
+console.log(population)
+
+let populationBig = population * 3
+console.log(populationBig)
+
+const javaScriptIsCool = true
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+console.log(colors)
+
+for (let i=1; i<=5; i++) {
+    console.log(i)
+}
+
+for (let i=0; i<=4; i++) {
+    console.log(i)
+}

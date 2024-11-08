026338a5f7bb0a35336e6658353f97fc16f57794
Lia Nirta <nirta.lia@gmail.com>
2024-11-07 21:03:10 -0500

    add finished counting lab

--- Counting_Lab/script.js
+++ Counting_Lab/script.js
@@ -9,8 +9,35 @@ Create this array: const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Use a for…
 for (let i=1; i<=10; i++) {
     console.log(i)
 }
+
 for  (let i=10; i>=1; i--) {
     console.log(i)
 }
 
-let counter
+
+let counterUp = 1
+while (counterUp <= 10) {
+    console.log(counterUp++)
+}
+
+let counterDown = 10
+while (counterDown >= 1) {
+    console.log(counterDown--)
+}
+
+
+let doCounterUp = 1
+do {
+    console.log(doCounterUp++)
+} while (doCounterUp <= 10)
+
+let doCounterDown = 10
+do {
+    console.log(doCounterDown--)
+} while (doCounterDown >= 1)
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for (const number of numbers) {
+        console.log(number)
+    }

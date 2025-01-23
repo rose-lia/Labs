e6d7a32ef8acf017eb32beacd9c6be45603131a2
Lia Nirta <nirta.lia@gmail.com>
2025-01-23 10:26:39 -0500

    fixed racecar lab commit issue

---
+++ Racecar_Lab/tests/SolarCar.test.ts
@@ -0,0 +1,20 @@
+import { Racer } from "../Racer";
+import { SolarCar } from "../SolarCar";
+
+describe("SolarCar Class", () => {
+    it("the team property is set from the constructor parameter", () => {
+
+    })
+    it("the speed property starts at 0", () => {
+        
+    })
+    it("calling accelerate once brings the speed to 1", () => {
+        
+    })
+    it("calling accelerate twice brings the speed to 2", () => {
+        
+    })
+    it("isFuelEmpty returns false", () => {
+        
+    })
+})
